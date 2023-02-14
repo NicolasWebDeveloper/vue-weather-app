f9537ddf1456d6f735022fe7b9632b352104e2ec
NicolasWebDeveloper <nicolas.duernbeck@gmx.at>
2023-02-14 09:02:55 +0100

    Initial 🎉

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <app-container></app-container>
+</template>
+
+<script>
+import AppContainer from './components/AppContainer.vue';
+
+export default {
+  components: { AppContainer },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #0a0c0d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/AppContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+div {
+  width: 50vw;
+  height: 45vh;
+  background-color: #fff;
+  border-radius: 1rem;
+}
+</style>
